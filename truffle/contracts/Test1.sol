b10feb39c5e7566fd0df64646073f76b7f9d6a4d
David Rowe <david@funkin.com>
2017-10-10 09:24:46 +0100

    A fair amount of rework, especially with the Test2 contract.  Still getting my head round its exact functionality and getting the functions in place to support it.  Not yet working.

--- truffle/contracts/Test1.sol
+++ truffle/contracts/Test1.sol
@@ -1,120 +1,161 @@
-pragma solidity ^0.4.4;
+pragma solidity ^0.4.14;
 
 contract Test1 {
 
-   struct organisationData {
-       string name;
-       State currentState;
-       uint questionCount;
-       string []questions;
-       bool isAvailable;
-   }
-
   enum State { Development, Live, Archived }
+
+  // a single 'organisation' and their set of questions
+  struct organisationData {
+    string name;
+    State currentState; // dev/live/archived
+    uint questionCount; // items in questions array
+    string []questions;
+    bool isAvailable; // used to just test if an organisation has been added
+  }
   
-   address contractOwner;
-   mapping (address=>organisationData) public OrgData;
+  address contractOwner; // who created this overall contract
+  
+  // each account has a single 'organisation'
+  mapping (address=>organisationData) public orgData;
 
+  function Test1() public {
+    contractOwner = msg.sender;
+  }
 
-   function Test1() public {
-     contractOwner = msg.sender;
-   }
+  modifier ifCreator() {
+    if (msg.sender == contractOwner) {
+      _;
+    } else {
+      revert();
+    }
+  }
    
-   
-   modifier ifCreator() {
-       if (msg.sender == contractOwner) {
-           _;
-       } else {
-           revert();
-       }
-   }
-   
-   modifier ifValidOrganisation() {
-       if (OrgData[msg.sender].isAvailable ) {
-           _;
-       } else {
-           revert();
-       }
-   }
+  // check if the organisation identified by their address alread has a question set
+  modifier ifValidOrganisation() {
+    if (orgData[msg.sender].isAvailable ) {
+      _;
+    } else {
+      revert();
+    }
+  }
 
-   modifier ifInDevelopmentState() {
-       if (OrgData[msg.sender].currentState == State.Development ) {
-           _;
-       } else {
-           revert();
-       }
-   }
+  // check if current organisation is in a development state (i.e., can have new questions added)
+  modifier ifInDevelopmentState() {
+    if (orgData[msg.sender].currentState == State.Development ) {
+      _;
+    } else {
+      revert();
+    }
+  }
+ 
+  function createOrganisation(string _name) ifCreator() public {
+    orgData[msg.sender].name = _name;
+    orgData[msg.sender].currentState = State.Development;
+    orgData[msg.sender].isAvailable = true;
+  }
 
-   
-   function createOrganisation(string name) ifCreator() public {
-       OrgData[msg.sender].name = name;
-       OrgData[msg.sender].currentState = State.Development;
-       OrgData[msg.sender].isAvailable = true;
-   }
+  // move from dev (or other state) to live
+  function setStateToLive() ifValidOrganisation() public {
+    orgData[msg.sender].currentState = State.Live;
+  }
 
-   function setStateToLive() ifValidOrganisation() public {
-       OrgData[msg.sender].currentState = State.Live;
-   }
-
-
-
-   function addQuestion(string questionToAdd) ifValidOrganisation() ifInDevelopmentState() public {
-       OrgData[msg.sender].questions.push(questionToAdd);
-       OrgData[msg.sender].questionCount++;
-   }
+  // add new questions - only if the question set is in development state
+  function addQuestion(string _questionToAdd) ifValidOrganisation() ifInDevelopmentState() public {
+    orgData[msg.sender].questions.push(_questionToAdd);
+    orgData[msg.sender].questionCount++;
+  }
 
   function getQuestionCount() constant public returns (uint) {
-      return OrgData[msg.sender].questionCount;
+    return orgData[msg.sender].questionCount;
   }
 
-  function getQuestionCount(address owner) constant public returns (uint) {
-      return OrgData[owner].questionCount;
+  function getQuestionCount(address _owner) constant public returns (uint) {
+    return orgData[_owner].questionCount;
   }
 
-  function getQuestion(uint questionNumber) ifValidOrganisation() constant public returns (string) {
-      return OrgData[msg.sender].questions[questionNumber];
+  function getQuestion(uint _questionNumber) ifValidOrganisation() constant public returns (string) {
+    return orgData[msg.sender].questions[_questionNumber];
   }
   
-  function getQuestion(address owner, uint questionNumber) constant public returns (string) {
-      return OrgData[owner].questions[questionNumber];
+  function getQuestion(address _owner, uint _questionNumber) constant public returns (string) {
+    return orgData[_owner].questions[_questionNumber];
   }
-    
 }
 
 
 
+// This contract is managed and controlled by a single contract owner.  For a given user
+// (indexed by a unique identifier) it will store a list of organisation addresses (an org
+// contains the questions) and the answers.
+contract Test2 { 
+  //Test1 t1 = Test1(0x62dc125741851e1e9a3d9e6623652e93b31ce8f2);
 
-contract Test2 {
-    //Test1 t1 = Test1(0x62dc125741851e1e9a3d9e6623652e93b31ce8f2);
+  address contractOwner; // who created this overall contract
+  
+  function Test2() public {
+    contractOwner = msg.sender;
+  }
 
-    struct userResponse {
-        address questionSetOwner;
-        bool[] answers;
+  modifier ifCreator() {
+    if (msg.sender == contractOwner) {
+      _;
+    } else {
+      revert();
     }
+  }
 
-    struct user {
-        string userId; // could be email address or email hash
-        userResponse[] userResponses;
-        bool isCreated;
-    }
+  struct userResponse {
+    address questionSetOrganisation; // organisation who owns the questions
+    bool[] answers;
+  }
 
-    mapping(address=>user) users;
+  struct user {
+    string userIdentifier; // could be email address or email hash
+    userResponse[] userResponses;
+    mapping(address=>userResponse) userResponseMap;
+    bool isCreated;
+  }
+  
+  
+
+//    mapping(address=>user) users;
+  mapping(string=>user) users;
     
-    // register a new user account - only called once per user
-    function registerUser(string _userId) public {
-        user thisUser = users[msg.sender];
-        if(thisUser.isCreated) {
-            revert();
-        }
+  // register a new user account - only called once per user
+  function registerUser(string _userId) ifCreator() public {
+//        user storage thisUser = users[msg.sender]; // get the user from the map of users
+    user storage thisUser = users[_userId]; // get the user from the map of users
+    if(thisUser.isCreated) {
+      revert();
+    }
         
-        thisUser.userId = _userId;
-        thisUser.isCreated = true;
-    }
+    thisUser.userIdentifier = _userId; // store the new users identifier
+    thisUser.isCreated = true; // mark as created
+  }
 
-    function isRegisteredUser(string _userId) public {
-        // 
-    }
-    
+  function registerOrganisationForUser(string _userId, address _organisationAddress) ifCreator() public {
+    bool[] memory userA;
+    userResponse memory userR = userResponse(_organisationAddress, userA);
+    userR.questionSetOrganisation = _organisationAddress;
+    users[_userId].userResponses.push(userR);
+  }
+
+  function getUserResponse(string _userId, address _organisationAddress, uint _questionNumber) ifCreator() public returns (bool) {
+    // for the user calling this contract, get the responses for the given organisation address
+   user u = users[_userId];
+   userResponse ur = u.userResponseMap[_organisationAddress];
+   bool ura = ur.answers[_questionNumber];
+   return ura;
+  }
+
+  function isRegisteredUser(string _userId) ifCreator() public constant returns (bool) {
+    user storage thisUser = users[_userId];
+    return thisUser.isCreated;
+  }
+
+  // register an answer for a question - can only be done if a question set is not in development (or indeed exists)
+  function registerAnswer(address questSetOrganisation) ifCreator() public {//, string question, bool answer) ifQuestionsLive() public {
+  }
 
 }
 
