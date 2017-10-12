7b6fa931fa621b028768e6adc86b18fd176b3ca4
David Rowe <david@funkin.com>
2017-10-12 11:40:07 +0100

    First version of a test script.  I'm getting 'invalid opcode' errors when running a test. I think its related to the owner of the contract when I try and step through using the remix debugger.

---
+++ truffle/test/TestTest1.sol
@@ -0,0 +1,38 @@
+pragma solidity ^0.4.6;
+
+import "truffle/Assert.sol";
+import "truffle/DeployedAddresses.sol";
+import "../contracts/Test1.sol";
+
+contract TestTest1 {
+  event LogSomething(string _output);
+
+  enum State { Development, Live, Archived }
+
+  function testOrganisationNotCreated() {
+    Test1 _t1 = Test1(DeployedAddresses.Test1());
+    var(_orgName, _orgState, _orgQuestionCount, _orgIsAvailable) = _t1.getOrgData(tx.origin);
+    Assert.isFalse(_orgIsAvailable, "Data for this organisation not yet created");
+  }
+
+
+  function testOrganisationCreated() {
+//    LogSomething("111");
+    Test1 _t1 = Test1(DeployedAddresses.Test1());
+//    LogSomething("222");
+	_t1.createOrganisation("Name");
+//    LogSomething("333");
+    //var(_orgName, _orgState, _orgQuestionCount, _orgIsAvailable) = _t1.getOrgData(tx.origin);
+    //Assert.isTrue(_orgIsAvailable, "Data for this organisation not yet created");
+    Assert.isTrue(true, "All OK");
+  }
+
+
+  function testTest1ContractOwner() {
+    Test1 _t1 = Test1(DeployedAddresses.Test1());
+    address _a = _t1.getContractOwner();
+	address _b = tx.origin;
+    Assert.equal(_a, _b, "Contract owner and TX origin not equal.");	
+  }
+
+}
