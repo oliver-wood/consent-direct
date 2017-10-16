38bad4a14da1b82accdd393f829ba2cb8db55f19
David Rowe <david@funkin.com>
2017-10-16 14:46:27 +0100

    Removed references to old test contracts.  Should just be Test1 and Test2 at present.

--- truffle/migrations/2_deploy_contracts.js
+++ truffle/migrations/2_deploy_contracts.js
@@ -3,14 +3,10 @@ var ConvertLib = artifacts.require("./ConvertLib.sol");
 
 var Test1 = artifacts.require("Test1");
 var Test2 = artifacts.require("Test2");
-var Test3 = artifacts.require("Test3");
-var Test4 = artifacts.require("Test4");
 
 module.exports = function(deployer) {
   deployer.deploy(ConvertLib);
 
  deployer.deploy(Test1);
  deployer.deploy(Test2);
- deployer.deploy(Test3);
- deployer.deploy(Test4);
 };
